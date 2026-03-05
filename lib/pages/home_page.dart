425f4ab13af8d04742d6b6d971a33b8db6a952d7
Olivier Pleynet <olivier.pleynet@atos.net>
2026-03-05 14:12:05 +0100

    Use dio package and listview. builder

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:dio/dio.dart';
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key});
@@ -10,33 +11,76 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyPageHomeState extends State<MyHomePage> {
+      final dio = Dio();
+    List pays = [];
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: Text("Home page")),
-      body: Center(
-        child: Container(
-          width: 400,
 
-          child: ListView(
-            padding: EdgeInsets.all(10),
-            children: [
-              ElevatedButton(
-                onPressed: () {
-                  Navigator.pushNamed(context, '/mail');
-                },
-                child: Text("Aller page mail"),
-              ),
-              Divider(height: 20,color: Colors.red,),
-              ElevatedButton(
-                onPressed: () {
-                  Navigator.pushNamed(context, '/settings');
-                },
-                child: Text("Aller page setting"),
-              ),
-            ],
-          ),
+
+ 
+    getCountries() async {
+      final response = await dio.get("https://restcountries.com/v3.1/all?fields=name,flags");
+      //print(response);    
+      setState(() {
+        pays = response.data;
+        print(pays);
+      });
+    }
+
+
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Home page"),
+        elevation: 18,
+        actions: [
+          IconButton(
+            onPressed: (){
+                getCountries();
+            }, 
+            icon: Icon(Icons.refresh)),
+        ],
         ),
+      body: Row(
+        children: [
+          SizedBox(
+            height: 500,
+            width: 300 ,
+            child: ListView.builder(
+              itemCount: pays.length,
+              itemBuilder: (context,Index){
+                return ListTile(
+                  leading: Image.network(pays[Index]['flags']['png'],width: 50,),
+                  title: Text(pays[Index]['name']['common']),
+                );
+            }),
+          ),
+          Center(
+            child: Container(
+              width: 400,
+          
+              child: ListView(
+                padding: EdgeInsets.all(10),
+                children: [
+                  Text("Bienvenu"),
+                  ElevatedButton(
+                    onPressed: () {
+                      Navigator.pushNamed(context, '/mail');
+                    },
+                    child: Text("Aller page mail"),
+                  ),
+                  Divider(height: 20,color: Colors.red,),
+                  ElevatedButton(
+                    onPressed: () {
+                      Navigator.pushNamed(context, '/settings');
+                    },
+                    child: Text("Aller page setting"),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
